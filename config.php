022b57168d8b1d2f3cdc64ccc1b620d978c85718
PravalikaThummanapelly <pravalikathummanapelly@gmail.com>
2026-04-07 17:49:04 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "user_management");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+session_start(); // ✅ only here
+?>
